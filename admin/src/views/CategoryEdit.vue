7565335a6ab45079fd7cc9bdef555140e7e3396f
lxj | 李显杰-前端事业部 | 前端 <474933555@qq.com>
2020-02-09 22:49:57 +0800

    分类

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>{{id ? '编辑' : '新建'}}分类</h1>
+    <el-form label-width="120px" @submit.native.prevent="seve">
+      <el-form-item label="名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: { type: String }
+  },
+  data() {
+    return {
+      model: {}
+    };
+  },
+  methods: {
+    async seve() {
+      //eslint-disable-next-line no-unused-vars
+      const res = await this.$http.post("categories", this.model);
+      this.$router.push("/categories/list");
+      this.$message({
+        type: "success",
+        message: "保存成功"
+      });
+    },
+    async fetch() {
+      const res = await this.$http.get(`categories/${this.id}`);
+      this.model = res.data;
+    }
+  },
+  created() {
+    this.id && this.fetch();
+  }
+};
+</script>
---
+++ admin/src/views/CategoryList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <el-table :data="items">
+      <el-table-column prop="_id" label="ID" width="260"></el-table-column>
+      <el-table-column prop="name" label="分类名称"></el-table-column>
+      <el-table-column label="操作" fixed="right" width="80">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            @click="$router.push(`/categories/edit/${scope.row._id}`)"
+          >编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  created() {
+    this.fetch();
+  },
+  methods: {
+    async fetch() {
+      const res = await this.$http.get("categories");
+      this.items = res.data;
+    }
+  }
+};
+</script>
