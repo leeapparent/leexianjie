39bff3b9fe935fd949bd0f7937c861e7e01ba691
lxj | 李显杰-前端事业部 | 前端 <474933555@qq.com>
2020-02-16 15:11:13 +0800

    上传

--- admin/src/main.js
+++ admin/src/main.js
@@ -7,6 +7,22 @@ Vue.config.productionTip = false
 
 
 import http from './http'
+
+
+Vue.mixin({
+  computed: {
+    uploadUrl() {
+      return this.$http.defaults.baseURL + '/upload'
+    }
+  },
+  methods: {
+    getAuthHeaders() {
+      return {
+        Authorization: `Bearer  ${localStorage.token || ''}`
+      }
+    }
+  }
+})
 Vue.prototype.$http = http
 new Vue({
   router,
